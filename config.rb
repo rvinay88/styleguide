e7e57b2f36acda1a21cc77db083cd182f99788d6
Vinay Raghu <rvinay88@gmail.com>
2013-06-03 10:17:52 +0530

    Sass source mapes enabled

--- config.rb
+++ config.rb
@@ -8,6 +8,7 @@ css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
+sass_options = { :debug_info => true }
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
